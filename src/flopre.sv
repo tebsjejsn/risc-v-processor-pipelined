9c2ef2ba2e2bc39e530cb0662dedabf6f35189c6
Tej Patel <tej.patel025@gmail.com>
2026-06-03 13:32:30 -0400

    feat(flopre.sv): add resettable flip-flop with enable

---
+++ src/flopre.sv
@@ -0,0 +1,15 @@
+module flopre
+#(
+    parameter width=32;
+) (
+    input  logic             clk,
+    input  logic             reset,
+    input  logic             en,
+    input  logic [width-1:0] d,
+    output logic [width-1:0] q
+);
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset)       q <= '0;
+        else if (enable) q <= d;
+    end
+endmodule
