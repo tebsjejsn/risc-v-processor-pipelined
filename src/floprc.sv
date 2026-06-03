e788ade4c596ba8fa40b9aa8be03af7c2a986320
Tej Patel <tej.patel025@gmail.com>
2026-06-03 13:33:24 -0400

    feat(floprc.sv): add resettable flip-flop with clear

---
+++ src/floprc.sv
@@ -0,0 +1,16 @@
+module floprc
+#(
+    parameter width=32;
+) (
+    input  logic clk,
+    input  logic reset,
+    input  logic clr,
+    input  logic [width-1:0] d,
+    output logic [width-1:0] q
+);
+    always_ff begin
+        if (reset)    q <= '0;
+        else if (clr) q <= '0;
+        else          q <= d;
+    end
+endmodule
