59fffe051bac8343ddc944b82ee524d0ea154817
Tej Patel <tej.patel025@gmail.com>
2026-06-14 22:17:43 -0400

    feat(floppc.sv): add flip-flop module specifically for the pc register

---
+++ src/floppc.sv
@@ -0,0 +1,15 @@
+module floppc(
+    input  logic        clk,
+    input  logic        reset,
+    input  logic        en,
+    input  logic [31:0] d,
+    output logic [31:0] q
+);
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) 
+            q <= 32'h80000000;
+        else if (en) 
+            q <= d;
+    end
+endmodule
